f52302aba53423d6d685b46dc5085e86231704df
jamilshafayat <jamilshafayat8@gmail.com>
2021-07-10 21:42:41 +0600

    project filtering

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 import ProjectsNavbar from "../components/ProjectNavbar";
 import { allProjects } from "../data";
+import { Category } from "../type";
 
-function projects() {
+const Projects = () => {
+	const [projects, setProjects] = useState(allProjects);
+	const [active, setActive] = useState("all");
+
+	const handlerFilterCategory = (category: Category | "all") => {
+		if (category === "all") {
+			setProjects(allProjects);
+			setActive(category);
+			return;
+		}
+
+		allProjects.filter((project) => project.category.includes(category));
+	};
+
 	return (
 		<div className="px-5 py-2 overflow-scroll" style={{ height: "65vh" }}>
 			<ProjectsNavbar />
@@ -18,6 +33,6 @@ function projects() {
 			</div>
 		</div>
 	);
-}
+};
 
-export default projects;
+export default Projects;
